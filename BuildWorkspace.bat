8658334c266614179b46e02764ce7c911728d4fd
Marcus Quigley <marcusquigley@gmail.com>
2021-06-17 16:38:47 -0400

    fixing getting truck by name

--- BuildWorkspace.bat
+++ BuildWorkspace.bat
@@ -11,3 +11,4 @@ dotnet build .\src\Services\Discount\Discount.Grpc\
 dotnet build .\src\Services\Catalog\Catalog.Api\
 dotnet build .\src\Services\Basket\Basket.Api\
 dotnet build .\src\Services\Orders\Orders.Api\
+dotnet build .\src\Services\Basket\Basket.Api\
