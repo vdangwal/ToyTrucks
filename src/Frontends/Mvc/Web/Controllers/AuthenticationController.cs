558624d0e638ed349634cf8f236c7ec4af3e4c42
Marcus Quigley <marcusquigley@gmail.com>
2021-11-01 13:45:52 -0400

    Logout from oidc

---
+++ src/Frontends/Mvc/Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace Web.Controllers
+{
+
+    public class AuthenticationController : Controller
+    {
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
