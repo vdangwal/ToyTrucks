09d7b5af8fb970cb7a155d299231913a3cc545e0
Marcus Quigley <marcusquigley@gmail.com>
2021-11-20 08:40:25 -0500

    Adding Identity

--- BuildWorkspace.bat
+++ BuildWorkspace.bat
@@ -11,6 +11,7 @@ dotnet build .\src\Services\Discount\Discount.Grpc\
 dotnet build .\src\Services\Catalog\Catalog.Api\
 dotnet build .\src\Services\Basket\Basket.Api\
 dotnet build .\src\Services\Orders\Orders.Api\
+dotnet build .\src\Security\Identity\
 @REM dotnet build .\src\Services\Inventory\Inventory.Api\
 dotnet build .\src\ApiGateways\Shopping.Aggregator\
 dotnet build .\src\ApiGateways\OcelotApi\
