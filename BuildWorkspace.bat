16658eefac0acfd7e69678a8b9d8031287d83a07
Marcus Quigley <marcusquigley@gmail.com>
2021-12-23 12:22:21 -0500

    removed errors after rep rename

--- BuildWorkspace.bat
+++ BuildWorkspace.bat
@@ -1,7 +1,7 @@
 
 echo 'Building projects...'
 dotnet build .\src\BuildingBlocks\EventBus.Messages\
-dotnet build .\src\Services\Discount\Discount.Grpc\
+@REM dotnet build .\src\Services\Discount\Discount.Grpc\
 dotnet build .\src\Services\Catalog\Catalog.Api\
 dotnet build .\src\Services\Basket\Basket.Api\
 dotnet build .\src\Services\Orders\Orders.Api\
